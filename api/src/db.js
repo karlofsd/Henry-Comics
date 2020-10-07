f480ebcc59edd5798deb0b8066ae960bc3c58f4b
cescontreras <contreras-cesaralejandro@hotmail.com>
2020-10-07 11:22:20 -0300

    db relation product-category

--- api/src/db.js
+++ api/src/db.js
@@ -31,10 +31,13 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Product, Categories } = sequelize.models;
+const { Product, Category } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
+Product.belongsToMany(Category, {through: 'ProductXCategory'});
+Category.belongsToMany(Product, {through: 'ProductXCategory'});
+
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
