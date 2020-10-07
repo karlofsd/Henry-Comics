24fd4f759f64ec04c4518365bde936586fbaa221
Alfredo <nzozez@gmail.com>
2020-10-07 19:41:58 -0300

    s17 s18 s19 s20

--- api/src/routes/categories.js
+++ api/src/routes/categories.js
@@ -1,3 +1,67 @@
 const server = require('express').Router();
 const { Category } = require('../db.js');
 
+//S18: Crear ruta para crear/agregar Categoria
+// res 201 Created. El request se ha competado y se a creado un nuevo recurso
+server.post('/', (req, res) => {
+    const { name, description} = req.body
+    Category.create({name, description})
+        .then((category) => {
+            res.status(201).json(category)
+        })
+})
+//S19: Crear Ruta para eliminar Categoria
+// res 200 OK. res Correcto
+// res 404 Not Found. 
+server.delete('/:id', (req, res) =>{
+    const id  =  req.params.id;
+    Category.destroy({
+        where: {
+            id: id
+        }
+    })
+    .then(succes =>{
+        if(succes){
+            res.status(200).json({message: 'Eliminado exitosamente'});
+        }
+    })
+    .catch(error =>{
+        res.status(404).json({message: 'Elemento no encontrado', error })
+    })
+})
+//S20: Crear ruta para Modificar Categoria
+// res 200 OK. res Correcto
+// res 404 Not Found. 
+server.put('/:id', (req, res) =>{
+    const id = req.params.id;
+    const { name, description, price, stock, image} = req.body
+    Category.update({
+        name: name,
+        description: description,
+        price: price,
+        stock: stock,
+        image: image
+    },
+    {
+        where: {
+            id:id
+        }
+    })
+    .then(succes =>{
+        if(succes){
+            res.status(200).json({message: 'Modificado'});
+        }
+    })
+    .catch(error =>{
+        res.status(404).json({message: 'El recurso de request no se a podido encontrar', error })
+    })
+})
+
+server.get('/', (req, res) =>{
+    Category.findAll()
+    .then(resp =>{
+        res.status(200).json(resp);
+    })
+})
+
+module.exports = server;
