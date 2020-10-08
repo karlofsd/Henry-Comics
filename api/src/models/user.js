21525ef5650fba20e026294826f72d5eb0d42cba
norma fiore <ignagni39@gmail.com>
2020-10-08 19:26:13 -0300

    Linea de orden

---
+++ api/src/models/user.js
@@ -0,0 +1,38 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('user', {
+    idUser: {
+      type: DataTypes.INTEGER,
+      primaryKey:true,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    phone: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+  });
+};
