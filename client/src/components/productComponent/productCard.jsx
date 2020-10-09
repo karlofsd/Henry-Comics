feeda9db9aca3e4d528a8fdca3071ff1e782c790
cescontreras <contreras-cesaralejandro@hotmail.com>
2020-10-09 17:54:28 -0300

    estilos ProductCard

--- client/src/components/productComponent/productCard.jsx
+++ client/src/components/productComponent/productCard.jsx
@@ -1,17 +1,34 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
-
+import './productCard.css'
 export default function ProductCard({product}) {
+    
+    
     return (
-    <div className='card shadow'>
-        <div>
-            <Link to={`/product/${product.id}`}>
-                <img src={product.image} alt="ImagenProducto"/>
-            </Link>
+        <div className='shadow wrapper'>
+            <div className="container">
+                <div className="top">
+                    <img src={product.image} className='reloj-img' alt="reloj" />
+                </div>
+                <div className="bottom">
+                <div className="left">
+                    <div className="details">
+                    <h4>{product.name = product.name.substring(0, 1).toUpperCase() + product.name.substring(1)}</h4>
+                    <p className='priceproductcard' >${product.price}</p>
+                    </div>
+                    <button /* onClick={() => handleClick()} */ className="buy" ><i className="fas fa-shopping-cart"></i></button>
+                </div>
+                </div>
+            </div>
+            <div className="container-hover">
+                <div className="top">
+                    <h3>{product.name}</h3>
+                    <p>{product.description}</p>
+                </div>
+                <div className="bottom">
+                <button /* onClick={() => handleClick()} */ className="buy" ><i className="fas fa-shopping-cart"></i></button>
+                </div>
+            </div>
         </div>
-        <div>
-            <h3>{product.name}</h3>
-            <h5>{product.price}</h5>
-        </div>
-    </div>);
+    );
 }
