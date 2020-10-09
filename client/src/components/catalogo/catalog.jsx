ed6b3a2812f2d97cf93f433cc6718193d54f7de7
Karlo Soberón <sober.karlo@gmail.com>
2020-10-09 10:12:00 -0300

    filterxcategory

--- client/src/components/catalogo/catalog.jsx
+++ client/src/components/catalogo/catalog.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ProductCard from '../productComponent/productCard.jsx'
+import axios from 'axios'
 
-export default function Catalog({products}) {
+export default function Catalog({products,id}) {
+    
+    const [filterProducts,setFilterProducts] = useState()
+
+    useEffect(() => {
+        if(id){
+            console.log('alert')
+            newFilter()
+        }else{
+            setFilterProducts(products)
+        }
+    },[])
+    
+    const newFilter = async() => {
+        const {data} = await axios.get(`http://localhost:3001/products/category/${id}`)
+        setFilterProducts(data)
+    }
+
     return (
     <div>
-       {products && products.map(p =><ProductCard product={p}/>)}
+       {filterProducts && filterProducts.map(p =><ProductCard product={p}/>)}
     </div>
     );
 }
