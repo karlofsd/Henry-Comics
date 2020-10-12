cc0c7157e51d953600e22be117f76894fd83ec17
Alfredo <nzozez@gmail.com>
2020-10-12 19:48:47 -0300

    carrito simple

--- client/src/components/searchBar/searchBar.js
+++ client/src/components/searchBar/searchBar.js
@@ -17,7 +17,6 @@ export default function Buscar({click}) {
 
     return (
         <form class="form-inline my-2 my-lg-0" onSubmit={handleSubmit}>
-         nuevoFooter
         <input class="form-control mr-sm-2" type="search" placeholder="Buscar..." aria-label="Search" value={searchText} onChange={handleChange}/>
         <button class="btn btn-outline-danger my-2 my-sm-0" type="submit" onClick={() => click(lowerCaseText)}>Buscar</button>
 
