8986dfcfb4d532d143cd31cdb6f8efbd1415c2ad
Alfredo <nzozez@gmail.com>
2020-10-12 23:12:45 -0300

    stylos Carrito

--- client/src/components/catalogo/catalog.jsx
+++ client/src/components/catalogo/catalog.jsx
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from 'react';
 import ProductCard from '../productComponent/productCard.jsx'
 import Filter from './filter/filter'
 import axios from 'axios'
+import Carrito from './carrito/carrito.jsx'
 import './catalog.css'
-import './carrito.css'
 
 export default function Catalog({products,id}) {
     const [filterProducts,setFilterProducts] = useState(products)
@@ -45,53 +45,8 @@ export default function Catalog({products,id}) {
         <div className='products'>
             {filterProducts && filterProducts.map(p =><ProductCard product={p}/>)}
         </div>
-        <div className='carrito'>
-            <h3 className='title-carrito'>Carrito</h3>
-            <div>
-                <ul className='list-carrito'>
-                    <label>Producto: </label>
-                    <li className='item-carrito'>
-                        <div className='lab-inp-but'>
-                            <label>item 1</label>
-                            <input className='inc-dec' type='number' min='1' step='1'/>
-                            <button className='end-but'>x</button>
-                        </div>
-                    </li>
-                    <li className='item-carrito'>
-                    <div className='lab-inp-but'>
-                            <label>item 2</label>
-                            <input className='inc-dec' type='number' min='1' step='1'/>
-                            <button className='end-but'>x</button>
-                        </div>
-                    </li>
-                    <li className='item-carrito'>
-                    <div className='lab-inp-but'>
-                            <label>item 3</label>
-                            <input className='inc-dec' type='number' min='1' step='1'/>
-                            <button className='end-but'>x</button>
-                        </div>
-                    </li>
-                </ul>
-                <ul className='list-carrito'>
-                    <label>Envio: </label>
-                    <li className='item-carrito'>
-                        <label>En Puerta</label>
-                        <input type= 'checkbox'></input>
-                    </li>
-                    <li className='item-carrito'>
-                        <label>A Sucursal</label>
-                        <input type= 'checkbox'></input>
-                    </li>
-                </ul>
-            </div>
-            <div className='total'>
-                <label>Producto + Envio</label>
-                <label>Total: $500</label>
-            </div>
-            <div className='buttons'>
-                <button>Comprar</button>
-                <button>Cancelar</button>
-            </div>
+        <div className= 'carrito'>
+            <Carrito />
         </div>
     </div>
     );
