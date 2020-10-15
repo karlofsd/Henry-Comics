3cdb1f2bd7b1a3eb7ef432f432aa79e1350caf92
Ignacio <54366484+auyeros@users.noreply.github.com>
2020-10-15 11:06:27 -0300

    Update User.js

--- api/src/models/User.js
+++ api/src/models/User.js
@@ -1,16 +1,34 @@
 const { DataTypes } = require('sequelize');
+const { validator } = require('sequelize/types/lib/utils/validator-extras');
 
 module.exports = (sequelize) => {
-    // defino el modelo
-    sequelize.define('user', {
-        email:{
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique:true,
-            validate: {
-				isEmail: true,
-			},
+
+  sequelize.define('user', {
+    firstname:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastname:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    username:{
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email:{
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true,
         }
-    });
-  };
+    },
+    password:{
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+
+  });
+}; 
   
