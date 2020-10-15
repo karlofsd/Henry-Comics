c5b214048cb77379aa6be10048bb2ab88f664d87
Nacho <auyerosignacio@gmail.com>
2020-10-15 02:26:40 -0300

    Agregadas rutas y modelo de User

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 // import all routers;
 const productRouter = require('./product.js');
 const categoryRouter = require ('./categories.js')
+const userRouter = require ('./user.js')
 
 
 const router = Router();
@@ -11,5 +12,6 @@ const router = Router();
 // router.use('/auth', authRouter);
 router.use('/products', productRouter);
 router.use('/category', categoryRouter);
+router.use('/user', userRouter);
 
 module.exports = router;
--- api/src/routes/user.js
+++ api/src/routes/user.js
@@ -10,58 +10,39 @@ User.findAll()
     .catch(next);
 })
 
-server.get("/:id", function (req, res) {
-    User.findOne({
-      where: {
-        id: req.params.id,
-      },
-      include: [
-        {
-          model: Orden,
-          as: "ordenes",
-        },
-      ],
-    })
-      .then(function (user) {
-        res.status(200).json(user);
-      })
-      .catch(function (reason) {
-        res
-          .status(404)
-          .json({ message: "No se obtuvo el usuario", data: reason });
-      });
-  });
 
-server.post("/add", function (req, res) {
-    var { name, email, password, adress } = req.body;
+
+server.post("/user/add", function (req, res) {
+    var { firstname, lastname, username, email, password } = req.body;
     User.create(
       {
-        name: name,
+        firstname: firstname,
+        lastname: lastname,
+        username: username,
         email: email,
         password: password,
-        adress: adress,
-        role: "user"
       },
       {
-        fields: ["name", "email", "password", "adress"],
+        fields: ["firstname", "lastname", "username", "email", "password"],
       }
     )
       .then(function (user) {
-        res
-          .status(200)
-          .json({ message: "Se creo correctamente el usuario", data: user });
+        res.status(200).json({ message: "Se creo correctamente el usuario", data: user });
       })
       .catch(function (err) {
         res.status(404).json({ err: "No se pudo crear el usuario", data: err });
       });
   });
 
-  server.put("/modify", function (req, res) {
-    const { id, name, adress } = req.body;
+  server.put("/:id/", function (req, res) {
+    const { id, firstname, lastname, username, email, password } = req.body;
     User.update(
       {
-        name: name,
-        adress: adress,
+        firstname: firstname,
+        username: username,
+        lastname: lastname,
+        email: email,
+        password: password
       },
       {
         where: {
@@ -69,54 +50,52 @@ server.post("/add", function (req, res) {
         },
         returning: true,
       }
-    ).then(function (respuesta) {
-      const user = respuesta[1][0];
-      res.status(200).json({ message: "Se cambio con exito", data: user });
-    });
+    ).then( (response) =>
+       {
+      res.status(200).json({ response, message: "Se cambio con exito"  })
+    }
+    ).catch( (err) => 
+    {
+      res.status(500).json({ err, message: "No se pudo cambiar" });
+    }
+    )
   });
 
-  server.put("/:id/passwordReset", function (req, res) {
-    const { password } = req.body;
+  server.delete("/:id", (req, res, next) => {
     const id = req.params.id;
-    var hash = bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
-    User.update(
-      {
-        password: hash,
-      },
-      {
-        where: {
-          id: id,
-        },
-        returning: true,
+    User.destroy({
+      where: { id: id },
+    }).then((removed) => {
+      if (removed) {
+      res.status(200).end();
+      } else {
+      res.status(404).json({ message: "Not found" });
       }
-    ).then(function (respuesta) {
-      const user = respuesta[1][0];
-      res.status(200).json({ message: "Se cambio con exito", data: user });
     });
-  });
-
-  server.post("/login", function (req, res) {
-    var { email, password } = req.body;
-    User.findOne({
-      where: {
-        email,
-      },
-    })
-      .then(function (user) {
-        bcrypt.compare(password, user.password).then(function (bool) {
-          if (bool) {
-            const token = jwt.sign({ email, password }, "...");
-            res.json({ message: "Se logueo el usuario", data: { token, user } });
-          } else {
-            res
-              .status(404)
-              .json({ success: false, message: "password incorrecta" });
-          }
-        });
-      })
-      .catch(function (err) {
-        res
-          .status(403)
-          .json({ message: "No se encontro el usuario.", data: err });
-      });
-  });
+    });
+    
+  // server.post("/login", function (req, res) {
+  //   var { email, password } = req.body;
+  //   User.findOne({
+  //     where: {
+  //       email,
+  //     },
+  //   })
+  //     .then(function (user) {
+  //       bcrypt.compare(password, user.password).then(function (bool) {
+  //         if (bool) {
+  //           const token = jwt.sign({ email, password }, "...");
+  //           res.json({ message: "Se logueo el usuario", data: { token, user } });
+  //         } else {
+  //           res
+  //             .status(404)
+  //             .json({ success: false, message: "password incorrecta" });
+  //         }
+  //       });
+  //     })
+  //     .catch(function (err) {
+  //       res
+  //         .status(403)
+  //         .json({ message: "No se encontro el usuario.", data: err });
+  //     });
+  // });
