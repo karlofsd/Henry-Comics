e9845c26c77c81a8804b13eb13fb465f4e735bd6
Karlo Soberón <sober.karlo@gmail.com>
2020-10-18 05:08:45 -0300

    estado local

--- client/src/redux/carrito.js
+++ client/src/redux/carrito.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 //CONSTANTES
 
 const GET_CARRITO = 'GET_CARRITO'
-
+const GET_LOCAL_CARRITO = 'GET_LOCAL_CARRITO'
 
 // STATE
 
@@ -24,6 +24,15 @@ export default function carritoReducer (state = initialState, action){
                     userId: action.payload.userId
                 }
             }
+        case GET_LOCAL_CARRITO:
+            return {
+                ...state,
+                carritoProducts: action.payload,
+                carritoInfo: {
+                    id: 0,
+                    userId: 'GUEST'
+                }
+            }
         default:
             return {
                 ...state
@@ -36,13 +45,21 @@ export default function carritoReducer (state = initialState, action){
 export const getCarrito = () => async(dispatch) => { 
      try{
         const {data} = await axios.get(`http://localhost:3001/user/${1}/cart`)
-        console.log(data)
         dispatch({
             type: GET_CARRITO,
             payload: data
         })
-   
+
      }catch(error){
         console.log(error)
      }
+}
+
+export const getLocalCarrito = () => (dispatch) => {
+    
+    const data = JSON.parse(localStorage.getItem('carrito'))
+    dispatch({
+        type: GET_LOCAL_CARRITO,
+        payload: data
+    })
 }
